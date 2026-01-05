2548a0930e9284a60757888e5d2eeccb59077717
Ghasen.Guesmi <ghasen.guesmi@takiacademyteam.com>
2026-01-05 16:50:36 +0100

    Add single spritesheet animation system

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import buildConfig from './buildConfig.json' assert { type: 'json' };
+import buildConfig from './buildConfig.json' with { type: 'json' };
 
 const basePath = buildConfig?.vite?.vitePath ?? '/Mines-Demo/';
 
@@ -18,10 +18,14 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    hmr: {
+      overlay: true
+    }
   },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.webp', '**/*.wav', '**/*.mp3']
 });
